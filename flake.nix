7ca6ce3f15bff3385fabe134a557b5d75734a780
Myles Wirth <regime_skylark.0y@icloud.com>
2026-03-22 09:47:19 -0400

    Fixes to reliabilitiy

--- flake.nix
+++ flake.nix
@@ -25,8 +25,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib;
 
-        # Get the latest stable toolchain via fenix
-        toolchain = fenix.packages.${system}.stable.withComponents [
+        toolchain = fenix.packages.${system}.latest.withComponents [
           "cargo"
           "rustc"
           "rust-src"
@@ -36,26 +35,29 @@
 
         craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
 
-        # Filter source to include .rs, shaders, and Cargo files
         src = lib.cleanSourceWith {
-          src = ./.;
+          src = craneLib.path ./.;
           filter =
             path: type:
-            let
-              base = baseNameOf path;
-              ext = lib.last (lib.splitString "." base);
-            in
-            (lib.hasSuffix ".rs" base)
-            || (lib.hasSuffix ".metal" base)
-            || (lib.hasSuffix ".wgsl" base)
-            || (lib.hasSuffix ".hlsl" base)
-            || (builtins.elem base [
-              "Cargo.toml"
-              "Cargo.lock"
-            ]);
+            (craneLib.filterCargoSources path type)
+            || (lib.hasSuffix ".metal" path)
+            || (lib.hasSuffix ".wgsl" path)
+            || (lib.hasSuffix ".hlsl" path)
+            || (lib.hasSuffix ".glsl" path);
         };
 
-        # Shared dependencies for build and shell
+        linuxLibs = with pkgs; [
+          alsa-lib
+          libdrm
+          mesa # provides libgbm
+          libxkbcommon
+          libva
+          vulkan-loader
+          wayland
+          xorg.libX11
+          xorg.libxcb
+        ];
+
         commonArgs = {
           inherit src;
           strictDeps = true;
@@ -74,41 +76,27 @@
               openssl
               zlib
             ]
-            ++ lib.optionals stdenv.isLinux [
-              alsa-lib
-              libdrm
-              libgbm
-              libxkbcommon
-              vulkan-loader
-              wayland
-              xorg.libX11
-              xorg.libxcb
-            ]
+            ++ lib.optionals stdenv.isLinux linuxLibs
             ++ lib.optionals stdenv.isDarwin [
               apple-sdk_15
-              (darwinMinVersionHook "10.15")
+              (darwinMinVersionHook "11.0")
             ];
 
-          # Essential for GPUI to find Vulkan/Wayland libs at runtime on Linux
           env = lib.optionalAttrs pkgs.stdenv.isLinux {
-            NIX_LDFLAGS = "-rpath ${
-              lib.makeLibraryPath (
-                with pkgs;
-                [
-                  vulkan-loader
-                  wayland
-                  libva
-                ]
-              )
-            }";
+            LD_LIBRARY_PATH = lib.makeLibraryPath linuxLibs;
           };
 
-          # GPUI usually requires runtime shaders for practical use
-          cargoExtraArgs = "--features gpui_platform/runtime_shaders";
+          cargoExtraArgs = "--features runtime_shaders";
         };
 
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        gpui = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+
+        gpui = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
       in
       {
         packages.default = gpui;
@@ -117,10 +105,12 @@
           inputsFrom = [ gpui ];
           packages = [ toolchain ];
 
-          # Realistic defaults for local development
           shellHook = ''
             export RUST_BACKTRACE=1
             export RUST_SRC_PATH="${toolchain}/lib/rustlib/src/rust/library"
+            ${lib.optionalString pkgs.stdenv.isLinux ''
+              export LD_LIBRARY_PATH="${lib.makeLibraryPath linuxLibs}:$LD_LIBRARY_PATH"
+            ''}
           '';
         };
       }
