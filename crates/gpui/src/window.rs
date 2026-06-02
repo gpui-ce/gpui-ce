3bce14ed714bd2582d703688f9367b2d4bc317e0
Yara 🏳️‍⚧️ <git@yara.blue>
2026-06-02 14:03:05 +0200

    Log worst hanging tasks and actions (#57835)
    We have a lot of long blocking tasks on both the foreground and
    background, this is a start of getting some insight into those.
    
    We will now log tasks running longer then 100ms on the foreground or
    background. Hanging actions will also be logged including their name. We
    simultaneously collect statistics on task and action performance and
    send those to telemetry. This includes quantiles and averages for each
    hanging task.
    
    Finally this adds tree dev actions:
    - hang action
    - hang foreground
    - hang background
    
    These cause a hang to check if hang reporting is working and in the
    future telemetry.
    
    Self-Review Checklist:
    
    - [x] I've reviewed my own diff for quality, security, and reliability
    - [x] Unsafe blocks (if any) have justifying comments
    - [x] The content is consistent with the [UI/UX
    checklist](https://github.com/zed-industries/zed/blob/main/CONTRIBUTING.md#uiux-checklist)
    - [x] Tests cover the new/changed behavior
    - [x] Performance impact has been considered and is acceptable
    
    Closes #ISSUE
    
    Release Notes:
    
    - Added logging and telemetry of tasks and actions with performance
    issues
    
    Upstream-commit: 39f7849a0fedc52def5ba8763344245f2a543532

--- crates/gpui/src/window.rs
+++ crates/gpui/src/window.rs
@@ -19,7 +19,7 @@ use crate::{
     TextStyleRefinement, ThermalState, TransformationMatrix, Transition, TransitionState,
     Underline, UnderlineStyle, WindowAppearance, WindowBackgroundAppearance, WindowBounds,
     WindowControls, WindowDecorations, WindowOptions, WindowParams, WindowTextSystem, point,
-    prelude::*, px, rems, size, transparent_black,
+    prelude::*, profiler, px, rems, size, transparent_black,
 };
 use anyhow::{Context as _, Result, anyhow};
 use collections::{FxHashMap, FxHashSet};
@@ -5127,7 +5127,9 @@ impl Window {
             .remove(&action.as_any().type_id())
         {
             for listener in &global_listeners {
+                profiler::update_running_action(action, cx);
                 listener(action.as_any(), DispatchPhase::Capture, cx);
+                profiler::save_action_timing();
                 if !cx.propagate_event {
                     break;
                 }
@@ -5157,7 +5159,9 @@ impl Window {
             {
                 let any_action = action.as_any();
                 if action_type == any_action.type_id() {
+                    profiler::update_running_action(action, cx);
                     listener(any_action, DispatchPhase::Capture, self, cx);
+                    profiler::save_action_timing();
 
                     if !cx.propagate_event {
                         return;
@@ -5177,7 +5181,9 @@ impl Window {
                 let any_action = action.as_any();
                 if action_type == any_action.type_id() {
                     cx.propagate_event = false; // Actions stop propagation by default during the bubble phase
+                    profiler::update_running_action(action, cx);
                     listener(any_action, DispatchPhase::Bubble, self, cx);
+                    profiler::save_action_timing();
 
                     if !cx.propagate_event {
                         return;
@@ -5194,7 +5200,9 @@ impl Window {
             for listener in global_listeners.iter().rev() {
                 cx.propagate_event = false; // Actions stop propagation by default during the bubble phase
 
+                profiler::update_running_action(action, cx);
                 listener(action.as_any(), DispatchPhase::Bubble, cx);
+                profiler::save_action_timing();
                 if !cx.propagate_event {
                     break;
                 }
